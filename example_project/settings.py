636ba95c4ade4ec6f9327fc30309dbc6fb328210
Abe <abe@habet.dev>
2024-05-18 22:50:00 -0400

    add django-extensions to example project

--- example_project/settings.py
+++ example_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "example_project.example",
     "django_stripe.checkout_session",
     "django_stripe.payment_intent",
